97f434c7811850dd511c42c01704897bb44e7a17
Steven Arce <stevenmatthew.arce@gmail.com>
2021-02-13 19:14:38 -0800

    Configured React to work with electron

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Inventory Tracker</h2>
+        </div>
+      </div>
+    )
+  }
+}
