a4f74872780853b6c7e5de795fc39709b51e35df
Steven Arce <stevenmatthew.arce@gmail.com>
2021-02-15 22:56:11 -0800

    Scaffolded project into frontend and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Sidenav } from './components';
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        
+      </div>
+    </Router>
+  )
+}
