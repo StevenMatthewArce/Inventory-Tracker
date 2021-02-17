b1812139f58e3d071764b0480e5302e478ffb77d
Steven Arce <stevenmatthew.arce@gmail.com>
2021-02-17 09:06:55 -0800

    Initialized sidebar

---
+++ frontend/src/components/Sidebar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Sidenav, Nav, Divider, Icon, Navbar } from 'rsuite';
+
+const panelStyles = {
+  padding: '15px 20px',
+  color: '#aaa'
+};
+
+const headerStyles = {
+  padding: 20,
+  fontSize: 16,
+  background: '#34c3ff',
+  color: '#fff'
+};
+
+const Sidebar = () => {
+  return (
+    <div style={{ width: 250 }}>
+      <Sidenav defaultOpenKeys={['3', '4']}>
+        <Sidenav.Header>
+          <div style={headerStyles}>Custom Sidenav</div>
+        </Sidenav.Header>
+        <Sidenav.Body>
+          <Nav>
+            <Nav.Item eventKey="1" active icon={<Icon icon="dashboard" />}>
+              <Link to="/inventory">Inventory Tracker</Link>
+            </Nav.Item>
+            
+          </Nav>
+        </Sidenav.Body>
+      </Sidenav>
+    </div>
+  )
+}
+
+export default Sidebar;
