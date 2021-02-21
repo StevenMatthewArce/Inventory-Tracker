9d93b12834ca7321f5fc172010e8ab5809eb0049
Steven Arce <stevenmatthew.arce@gmail.com>
2021-02-21 12:34:20 -0800

    Added firebase.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ import 'semantic-ui-css/semantic.min.css';
 
 class App extends React.Component {
   render() {
-    console.log(process.env.REACT_APP_API_KEY)
     return (
       <Router>
         <Grid columns={1}>
