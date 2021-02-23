3cc2fc5c041f58a79f56a75c6248ffe7342b6842
Steven Arce <stevenmatthew.arce@gmail.com>
2021-02-23 15:40:25 -0800

    Displaying inventory
    something wrong with react hooks in inventory.js

---
+++ src/components/InventoryComponents/AddItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class AddItem extends React.Component {
+  render() {
+    return (
+      <div>Add item</div>
+    )
+    
+  }
+}
+
+export default AddItem;
--- src/pages/Inventory.js
+++ src/pages/Inventory.js
@@ -1,46 +1,121 @@
-import React, { Component } from "react";
-import { Tab } from "semantic-ui-react";
+import React, { Component } from 'react';
+import _ from 'lodash';
 import {
-  Raw,
-  FinishedGoods,
-  Recipes
-} from "../components/Inventory_Components";
+  Button,
+  Modal,
+  Table,
+  TableBody
+} from 'semantic-ui-react';
+import { AddItem } from '../components/InventoryComponents';
 
-const panes = [
-  {
-    menuItem: "Raw Inventory",
-    render: () => (
-      <Tab.Pane>
-        <Raw />
-      </Tab.Pane>
-    )
-  },
-  {
-    menuItem: "Finished Goods",
-    render: () => (
-      <Tab.Pane>
-        <FinishedGoods />
-      </Tab.Pane>
-    )
-  },
-  {
-    menuItem: "Recipes",
-    render: () => (
-      <Tab.Pane>
-        <Recipes />
-      </Tab.Pane>
-    )
-  }
-];
+const tableData = [
+  { name: 'Eggs', description: 'Egg', cost: '12.20', quantity: '100', dateRestocked: '12/12/2020', notes: 'none'},
+  { name: 'Eggs', description: 'Egg', cost: '12.20', quantity: '100', dateRestocked: '12/12/2020', notes: 'none'},
+  { name: 'Eggs', description: 'Egg', cost: '12.20', quantity: '100', dateRestocked: '12/12/2020', notes: 'none'},
+  { name: 'Eggs', description: 'Egg', cost: '12.20', quantity: '100', dateRestocked: '12/12/2020', notes: 'none'}
 
+]
 class Inventory extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: {
+        uid: '12345',
+        name: 'default',
+        description: 'placeholder',
+        cost: '0',
+        quantity: '12',
+        dateRestocked: '12/12/2000',
+        notes: 'N/A',
+        photo: null
+      }
+    }
+  }
+
+  sort(state, action) {
+    switch (action.type) {
+      case 'CHANGE_SORT':
+        if (state.column === action.column) {
+          return {
+            ...state,
+            data: state.data.slice().reverse(),
+            direction:
+              state.direction === 'ascending' ? 'descending' : 'ascending',
+          }
+        }
+  
+        return {
+          column: action.column,
+          data: _.sortBy(state.data, [action.column]),
+          direction: 'ascending',
+        }
+      default:
+        throw new Error()
+    }
+  }
+
+
+  render() { 
+
+    const [state, dispatch] = React.useReducer(this.sort, {
+      column: null,
+      data: tableData,
+      direction: null,
+    })
+    const { column, data, direction } = state;
+
     return (
-      <Tab
-        menu={{ color: "black", inverted: true, pointing: true }}
-        panes={panes}
-      />
-    );
+      <div>
+        <Table sortable celled fixed>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell
+                sorted={column === 'name' ? direction : null}
+                onClick={() => dispatch({ type: 'CHANGE_SORT', column: 'name'})}
+              >
+                Name
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'description' ? direction : null}
+                onClick={() => dispatch({ type: 'CHANGE_SORT', column: 'description'})}
+              >
+                Description
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'cost' ? direction : null}
+                onClick={() => dispatch({ type: 'CHANGE_SORT', column: 'cost'})}
+              >
+                Cost
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'quantity' ? direction : null}
+                onClick={() => dispatch({ type: 'CHANGE_SORT', column: 'quantity'})}
+              >
+                Quantity
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'dateRestocked' ? direction : null}
+                onClick={() => dispatch({ type: 'CHANGE_SORT', column: 'dateRestocked'})}
+              >
+                Date
+              </Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {data.map(({ name, description, cost, quantity, dateRestocked, notes }) => (
+              <Table.Row key={name}>
+                <Table.Cell>{name}</Table.Cell>
+                <Table.Cell>{description}</Table.Cell>
+                <Table.Cell>{cost}</Table.Cell>
+                <Table.Cell>{quantity}</Table.Cell>
+                <Table.Cell>{dateRestocked}</Table.Cell>
+                <Table.Cell>{notes}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      </div>
+    )
   }
 }
 
