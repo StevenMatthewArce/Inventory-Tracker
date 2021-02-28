e83d25cbeeda8242b60e67282449ac4c14a940f8
sooada <72992901+sooada@users.noreply.github.com>
2021-02-27 20:46:17 -0800

    test

---
+++ src/pages/Budget.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Budget extends Component {
+  render() {
+    return (
+      <div>
+        Budget Tracker
+        hello
+      </div>
+    )
+  }
+}
+
+export default Budget;
