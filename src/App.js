c25198a4a16f203b4fb92bf6b5244e7a26c15896
Steven Arce <stevenmatthew.arce@gmail.com>
2021-03-02 23:26:47 -0800

    Added form component

--- src/App.js
+++ src/App.js
@@ -54,6 +54,9 @@ class App extends React.Component {
                     <Route path='/settings'>
                       <Settings />
                     </Route>
+                    <Route path='/addItem'>
+                      <AddItem />
+                    </Route>
                   </Switch>
                 </Segment>
               </Sidebar.Pusher>
