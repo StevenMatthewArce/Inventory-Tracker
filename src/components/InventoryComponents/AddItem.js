d1405668657915bdc5ae4da2097f97bfe0882b6a
Steven Arce <stevenmatthew.arce@gmail.com>
2021-03-02 21:40:27 -0800

    added sample data to additem

--- src/components/InventoryComponents/AddItem.js
+++ src/components/InventoryComponents/AddItem.js
@@ -1,6 +1,34 @@
 import React from 'react';
+import firebase from '.../firebase.js';
 
 class AddItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: {
+        uid: null,
+        name: null,
+        description: null,
+        cost: null,
+        quantity: null,
+        dateRestocked: null,
+        notes: null,
+        photo: null
+      }
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
   render() {
     return (
       <div>Add item</div>
