d890331142a74fbcc68c319bd668a329cc821eee
Steven Arce <stevenmatthew.arce@gmail.com>
2021-03-03 13:00:31 -0800

    Added methods for storing items in firebase

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,5 @@
-import firebase from 'firebase';
-
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -10,6 +10,21 @@ const config = {
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
+
 firebase.initializeApp(config);
 
+export const handleAddItem = (e, name, cost, quantity, dateRestocked) => {
+  e.preventDefault();
+  const ref = firestore.doc('items');
+
+  ref.add({
+    name: name,
+    cost: cost,
+    quantity: quantity,
+    dateRestocked: dateRestocked
+  })
+};
+
 export default firebase;
+
+export const firestore = firebase.firestore();
