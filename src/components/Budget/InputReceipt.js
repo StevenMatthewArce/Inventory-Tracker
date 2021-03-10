d701b21934e06319d7ca4a4e452ebb262ecb023e
Denil <denilpoude@gmail.com>
2021-03-09 21:38:14 -0800

    Added function to parse OCR data, need to pass back to parent

--- src/components/Budget/InputReceipt.js
+++ src/components/Budget/InputReceipt.js
@@ -1,13 +1,85 @@
 import React, { Component } from "react";
 import ImageLoader from "./ImageLoader";
+import UserDetails from "./UserDetails";
+import PersonalDetails from "./PersonalDetails";
+import Confirmation from "./Confirmation";
+import Success from "./Success";
 
 export class InputReceipt extends Component {
+  state = {
+    name: [""],
+    description: "",
+    cost: "",
+    quantity: "",
+    dateRestocked: "",
+    notes: "",
+    step: 1
+    // firstName: "",
+    // lastName: "",
+    // email: "",
+    // age: "",
+    // city: "",
+    // country: ""
+  };
+
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    });
+  };
+
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1
+    });
+  };
+
+  handleChange = input => event => {
+    this.setState({ [input]: event.target.value });
+  };
+
   render() {
-    return (
-      <div>
-        <ImageLoader />
-      </div>
-    );
+    const { step } = this.state;
+    const {
+      name,
+      description,
+      cost,
+      quantity,
+      dateRestocked,
+      notes
+    } = this.state;
+    const values = { name, description, cost, quantity, dateRestocked, notes };
+    switch (step) {
+      case 1:
+        return (
+          <ImageLoader
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return (
+          <PersonalDetails
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 3:
+        return (
+          <Confirmation
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            values={values}
+          />
+        );
+      case 4:
+        return <Success />;
+    }
   }
 }
 
