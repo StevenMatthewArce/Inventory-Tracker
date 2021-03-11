0b62bb8b83ccc59004efe8d939b016c9ea65b26f
Steven Arce <stevenmatthew.arce@gmail.com>
2021-03-11 12:30:50 -0800

    Reorganized inventory components

--- src/components/InventoryComponents/index.js
+++ src/components/InventoryComponents/index.js
@@ -1,1 +1,4 @@
+export { default as Raw } from './Raw';
+export { default as FinishedGoods } from './FinishedGoods';
+export { default as Recipes } from './Recipes';
 export { default as AddItem } from './AddItem';
