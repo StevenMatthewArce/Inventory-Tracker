9872fdb4423505bd03528831b5ac411517779fa9
Steven Arce <stevenmatthew.arce@gmail.com>
2021-03-11 12:14:11 -0800

    Revert "Rescaffolded InventoryComponents"
    This reverts commit 422cb825d38e8106aff2a40436a7e9f68ab8ee7b.

--- src/components/InventoryComponents/index.js
+++ src/components/InventoryComponents/index.js
@@ -1,3 +1,1 @@
-export { default as Raw } from './Raw';
-export {default as FinishedGoods} from './FinishedGoods';
-export { default as Recipes } from './Recipes';
+export { default as AddItem } from './AddItem';
