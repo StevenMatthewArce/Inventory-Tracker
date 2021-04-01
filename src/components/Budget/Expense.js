f8d7f02073e25222a6042a7b250c132909f1ac3e
Denil <denilpoude@gmail.com>
2021-03-31 18:14:06 -0700

    Added bar graphs, bar graphs respond to sales and profts

--- src/components/Budget/Expense.js
+++ src/components/Budget/Expense.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
-import { Table, Dropdown, Grid, Header, Divider } from "semantic-ui-react";
+import {
+  Table,
+  Dropdown,
+  Grid,
+  Header,
+  Divider,
+  Progress,
+  Statistic
+} from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import { db } from "../Firebase";
 import _ from "lodash";
+import { ExpenseGraphs } from "./ExpenseGraphs";
 
 //TODO: Add Expense Chart-Bar at the top
 //TODO: Add Filters
@@ -15,7 +24,12 @@ export class Expense extends Component {
     this.state = {
       data: [],
       column: null,
-      direction: null
+      direction: null,
+      totalExpenseMonth: null,
+      totalExpenseYear: null,
+      totalSalesYear: null,
+      expenseMonthPercentage: null,
+      expenseYearPercentage: null
     };
   }
 
@@ -26,10 +40,13 @@ export class Expense extends Component {
       .then(querySnapshot => {
         querySnapshot.forEach(doc => {
           documents.push({ ...doc.data(), id: doc.id });
-          console.log(doc.id, " => ", doc.data());
+          // console.log(doc.id, " => ", doc.data());
         });
       })
-      .then(() => this.setState({ data: documents }));
+      .then(() => this.setState({ data: documents }))
+      .then(() => this.calculateExpenseMonth());
+
+    this.calculateExpenseYear();
   }
 
   //TODO: Add sorting alg
@@ -51,11 +68,100 @@ export class Expense extends Component {
     });
   };
 
+  calculateExpenseMonth = () => {
+    //TODO: Add code to calculate monlth expenses
+
+    //! This is only for testing
+    let totalExpenseMonth = "1652";
+    let totalExpenseYear = "10000";
+    this.setState(
+      {
+        totalExpenseMonth: totalExpenseMonth,
+        totalExpenseYear: totalExpenseYear
+      },
+      console.log(this.state)
+    );
+
+    let expenseMonthPercentage = (totalExpenseMonth / totalExpenseYear) * 100;
+    this.setState({ expenseMonthPercentage: expenseMonthPercentage });
+  };
+
+  calculateExpenseYear = () => {
+    //TODO: Add code to calculate monthly expenses
+
+    //! This is only for testing
+    let totalSalesYear = "90000";
+    this.setState(
+      {
+        totalSalesYear: totalSalesYear
+      },
+      console.log(this.state)
+    );
+
+    let expenseYearPercentage =
+      (this.state.totalExpenseYear / totalSalesYear) * 100;
+    this.setState({ expenseYearPercentage: expenseYearPercentage });
+  };
+
   render() {
     const { data, column, direction } = this.state;
     console.log(this.state.data);
     return (
       <div style={{ height: "100vh" }}>
+        <div>
+          <Grid centered columns={2}>
+            <Grid.Column>
+              <Grid.Row verticalAlign="top">
+                <Statistic
+                  size="mini"
+                  color="orange"
+                  value={this.state.totalExpenseMonth}
+                  label="Total Expense This Month"
+                />
+
+                <Statistic
+                  floated="right"
+                  size="mini"
+                  color="grey"
+                  value={this.state.totalExpenseYear}
+                  label="Total Expense This Year"
+                />
+              </Grid.Row>
+
+              <Grid.Row verticalAlign="bottom">
+                <Progress
+                  color="orange"
+                  percent={this.state.expenseMonthPercentage}
+                />
+              </Grid.Row>
+            </Grid.Column>
+            <Grid.Column>
+              <Grid.Row verticalAlign="top">
+                <Statistic
+                  size="mini"
+                  color="olive"
+                  value={this.state.totalExpenseYear}
+                  label="Total Expense This Year"
+                />
+
+                <Statistic
+                  floated="right"
+                  size="mini"
+                  color="grey"
+                  value={this.state.totalSalesYear}
+                  label="Total Sales"
+                />
+              </Grid.Row>
+
+              <Grid.Row verticalAlign="bottom">
+                <Progress
+                  color="olive"
+                  percent={this.state.expenseYearPercentage}
+                />
+              </Grid.Row>
+            </Grid.Column>
+          </Grid>
+        </div>
         <div>
           <Grid columns="equal">
             <Grid.Column width={12}>
@@ -119,7 +225,6 @@ export class Expense extends Component {
                 </Table.HeaderCell>
               </Table.Row>
             </Table.Header>
-            {console.log(this.state)}
 
             {data.map(items => {
               return (
