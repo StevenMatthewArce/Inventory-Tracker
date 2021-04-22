4c58ce22d6cd5663336dd210de270facadab9d5b
Steven Arce <stevenmatthew.arce@gmail.com>
2021-04-22 11:26:01 -0700

    Added finished goods

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,7 +1,7 @@
 import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseApp = firebase.initializeApp({
+var firebaseApp = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
@@ -10,10 +10,11 @@ var firebaseApp = firebase.initializeApp({
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID
-});
+};
 
-var db = firebaseApp.firestore();
-const projectFirestore = firebase.firestore();
+firebase.initializeApp(firebaseApp);
+
+const db = firebase.firestore();
 const storage = firebase.storage();
 
-export { db, projectFirestore, storage };
+export { db, storage };
