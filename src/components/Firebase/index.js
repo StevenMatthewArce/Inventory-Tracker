967bd36e7db70a99e040540b64f1fa851ee1a34e
Steven Arce <stevenmatthew.arce@gmail.com>
2021-04-26 21:58:47 -0700

    Added auth methods

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,5 +1,8 @@
 import firebase from "firebase";
 
+import 'firebase/auth';
+import 'firebase/firestore';
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseApp = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -16,5 +19,64 @@ firebase.initializeApp(firebaseApp);
 
 const db = firebase.firestore();
 const storage = firebase.storage();
+const auth = firebase.auth();
 
-export { db, storage };
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+const googleSignIn = () => {
+  auth.signInWithRedirect(googleProvider).then((res) => {
+    console.log(res.user);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  })
+}
+
+const signUpWithEmailAndPassword = ( userData ) => {
+  const email = userData.email;
+  const password = userData.password;
+
+  auth.createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      const id = userCredential.user.uid;
+      db.collection('users').doc(id).set({
+        name: userData.name,
+        email: userData.email,
+        password: userData.password,
+        shopName: userData.shopName
+      })
+      return 'User has been signed up';
+    })
+    .catch((error) => {
+      return error.message;
+    });
+}
+
+const loginWithEmailAndPassword = (email, password) => {
+  auth.signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      return 'Signed in';
+    })
+    .catch((error) => {
+      return error.message;
+    })
+}
+
+const signOut = () => {
+  auth.signOut()
+    .then(() => {
+      return 'Signed out';
+    })
+    .catch((error) => {
+      return error.message;
+    })
+}
+
+export { 
+  db, 
+  storage, 
+  auth,
+  googleSignIn,
+  signUpWithEmailAndPassword,
+  signOut,
+  loginWithEmailAndPassword,
+};
