13fbfbc0e6eebae65079ffcc4dc703377e1a2236
sooada <72992901+sooada@users.noreply.github.com>
2021-04-28 20:13:10 -0700

    should get data?

--- src/components/Budget/expenseVisual.js
+++ src/components/Budget/expenseVisual.js
@@ -1,18 +1,121 @@
 import React from "react";
 import { VictoryAxis, VictoryBar, VictoryChart, VictoryTheme, VictoryContainer } from 'victory';
+import { db } from "../Firebase";
 import { RawMaterials, FinishedGoods, Recipes } from "../Inventory";
 
-
-function selectingData(){
-  firebase.database().ref('items').on('value',
-  function(AllRecords){
-    AllRecords.forEach(
-      function(CurrentRecord){
-        var quantity = CurrentRecord.val().quantity;
-      }
-    )
-  })
+function getData(){
+  //let datas = null;
+  db.collection('items')
+    .onSnapshot(snap=>{
+      let documents = [];
+      snap.forEach(doc=>{
+        documents.push({...doc.data(),id:doc.id});
+        //renderTable(doc);
+      })
+      renderTable(documents);
+      //data: catagories
+    })
 }
+
+function renderTable(documents){
+  var catagories = _.groupBy(documents, items =>{
+    return items.name;
+  });
+  let data = catagories
+  {Object.keys(data).map((value,index)=>{
+    const items = data[value];
+    let totalCost = 0;
+
+    items.forEach(element => {
+      totalCost += parseFloat(element.cost);
+    })
+    totalCost = totalCost.toFixed(2);
+  })};
+
+  //Insert Graph
+  //render() {
+    return (
+      <div style={{ height: '100vh' }}>
+        <h1>Visualization</h1>
+          <VictoryChart
+            //adding the material theme provided with victory
+            theme={VictoryTheme.material}
+            // domainPadding will add space to each side of VictoryBar to 
+            // prevent it from overlapping the axis
+            domainPadding={20}
+            height={300} width={400}
+            containerComponent={<VictoryContainer responsive={false}/>}
+          >
+
+            <VictoryAxis
+              // tickValues specifies both the bumber of ticks and where
+              // they are placed on the axis
+              tickValues={[1, 2, 3, 4]}
+              tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+            />
+            <VictoryAxis
+              dependentAxis
+              // tickFormat specifies how ticks should be displayed
+              tickFormat={(x) => `$${x/1000}k`}
+            />
+
+            <VictoryBar
+              data={data}
+              // data accessor for x values
+              x="data.name"
+              // data accessor for y values
+              y="data.totalCost"
+            />
+            </VictoryChart>
+
+            
+            <VictoryChart
+              //adding the material theme provided with victory
+              theme={VictoryTheme.material}
+              // domainPadding will add space to each side of VictoryBar to 
+              // prevent it from overlapping the axis
+              domainPadding={20}
+              height={300} width={400}
+              containerComponent={<VictoryContainer responsive={false}/>}
+            >
+
+              <VictoryAxis
+                // tickValues specifies both the bumber of ticks and where
+                // they are placed on the axis
+                tickValues={[1, 2, 3, 4]}
+                tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+              />
+              <VictoryAxis
+                dependentAxis
+                // tickFormat specifies how ticks should be displayed
+                tickFormat={(x) => `${x}%`}
+              />
+
+              <VictoryBar
+                data={inventory}
+                // data accessor for x values
+                x="quarter"
+                // data accessor for y values
+                y="goodsAmt"
+              />
+              </VictoryChart>
+        </div>
+      )
+    }
+
+
+}
+
+// function selectingData(){
+//   firebase.database().ref('items').on('value',
+//   function(AllRecords){
+//     AllRecords.forEach(
+//       function(CurrentRecord){
+//         var quantity = CurrentRecord.val().quantity;
+//       }
+//     )
+//   })
+// }
 
 const profit = [
     {quarter: 1, earnings: 1500},
@@ -29,7 +132,7 @@ const inventory = [
   ];
   
   class expenseVisual extends React.Component {
-    componentDidMount() {
+    //componentDidMount() {
       //!Uncomment this when firebase is back up
       db.collection("items")
         .onSnapshot(snap => {
@@ -43,14 +146,15 @@ const inventory = [
           this.setState({
             data: catagories
           });
-        });
+        }
+        );
       //!Uncomment this when firebase is back up
       // var catagories = _.groupBy(data, items => {
       //   return items.name;
       // });
       // // console.log(catagories);
       // this.setState({ data: catagories });
-    }
+    //}
 
 
     render() {
