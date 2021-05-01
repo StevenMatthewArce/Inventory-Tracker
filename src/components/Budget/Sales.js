7b88fe5bc1c1479701d63c749780a7489c3ebfe6
Denil <denilpoudel@gmail.com>
2021-04-30 21:12:15 -0700

    Sales page added

--- src/components/Budget/Sales.js
+++ src/components/Budget/Sales.js
@@ -1,23 +1,38 @@
 import React, { Component } from "react";
+import {
+  Table,
+  Dropdown,
+  Grid,
+  Header,
+  Divider,
+  Progress,
+  Statistic,
+  Button,
+  Checkbox,
+  Icon,
+  Modal,
+  Image,
+  Segment,
+  Message
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
 import { db } from "../Firebase";
-import { VictoryLine, VictoryChart, VictoryTheme, VictoryAxis } from "victory";
-import _, { flatMap, floor, negate } from "lodash";
 
-export default class Sales extends Component {
-      
-  constructor(){
-    super();
+export class Sales extends Component {
+  constructor(props) {
+    super(props);
 
-     this.state = {
-       data : [],
-       graphData: []
-      //data: [{x: Object.keys(idk),y:Object.values(idk)} ],
-      //data: [{x:"sunday", y: 3},{x:"mon", y:4}]
-    }
+    this.state = {
+      data: [],
+      checked: [],
+      error: null,
+      message: null
+    };
   }
+
   componentDidMount() {
     let documents = [];
-    db.collection("receipts")
+    db.collection("sales")
       .get()
       .then(querySnapshot => {
         querySnapshot.forEach(doc => {
@@ -25,74 +40,138 @@ export default class Sales extends Component {
           // console.log(doc.id, " => ", doc.data());
         });
       })
-      .then(() => this.setState({ data: documents }))
-      .then(() => this.calculateExpenseMonth());
-    }  
+      .then(() => {
+        let data = [...documents.map(element => element[0])];
 
-      calculateExpenseMonth=()=>{
-        const{data} = this.state;
-        
-        let monthArr = new Array(12).fill(0);
-        for(let k =0 ; k<monthArr.length; k++){
-          let totalExpenseMonth = 0;
-          //let totalExpenseYear = 0;
-          var currentdate = new Date();
-          var cur_month = k+1;
-          var cur_year = currentdate.getFullYear();
-          data.filter(element => {
-            let parts = element.date.split(/[/ :]/);
-            let month = parts[0];
-            let year = parts[2];
-            if (cur_month == month && year == cur_year) {
-              totalExpenseMonth += parseFloat(element.totalCost);
-            }
-            // if (year == cur_year) {
-            //   totalExpenseYear += parseFloat(element.totalCost);
-            // }
+        data.map((element, index) => {
+          element.SaleId = documents[index].id;
+          element.soldDate = documents[index].currentdate;
+        });
+        this.setState({ data: data });
+      });
+  }
+
+  toggleCheck = (e, { id }) => {
+    let { checked } = this.state;
+
+    if (checked.includes(id)) {
+      console.log("it already in arry");
+      const updatedArray = checked.filter(s => s !== id);
+      this.setState({ checked: updatedArray, error: null, message: null });
+    } else {
+      console.log(" not in arry");
+      checked.push(id);
+      this.setState({ checked: checked, error: null, message: null });
+    }
+  };
+
+  removeItem = () => {
+    let { checked } = this.state;
+    if (checked.length < 1) {
+      this.setState({ error: "Please Check a Sale" });
+    } else {
+      checked.map(element => {
+        db.collection("sales")
+          .doc(element)
+          .delete()
+          .then(() => {
+            console.log("Document successfully deleted!");
+            this.setState({ message: "Sucessfully removed Sale" });
           })
-          monthArr[k] = (totalExpenseMonth);
-        }
-        var graphDatal = [];
-        for(let k in monthArr){
-          let months = ['Jan','Feb','Mar', 'Apr', 'May','June','July','Aug', 'Sep','Oct', 'Nov', 'Dec'];
-          //let m = +k+ +1;
-          graphDatal.push({x:months[k], y:monthArr[k]})
-        }
-        this.setState({graphData: graphDatal});
-        };
-  
-  render(){
-    return(
-      <div>
-        <h1>Expense Line Chart</h1>
-        <VictoryChart>
-         <VictoryAxis
-              label = "Months"
-            />
-            <VictoryAxis
-              dependentAxis
-              // tickFormat specifies how ticks should be displayed
-              tickFormat={(x) => `$${x}`}
-            /> 
-          <VictoryLine data = {this.state.graphData}
-          height = {10}
-          //interpolation="natural"
-          labels={({ datum }) => datum.y}
-          theme={VictoryTheme.material}
-          style={{
-            data: {
-              stroke: "#02B875"
-            }
-          }}/>
-          {/* height={1} width={4} */}
-        </VictoryChart>
-      </div>
-    )
+          .catch(error => {
+            console.error("Error removing document: ", error);
+            this.setState({ error: "Error removing doucment" });
+          });
+      });
+    }
+  };
+
+  render() {
+    const { data } = this.state;
+    return (
+      <Segment style={{ height: "100vh" }}>
+        <div>
+          {this.state.error && (
+            <Message icon="frown" negative>
+              {this.state.error}
+            </Message>
+          )}
+        </div>
+        <br />
+        <div>
+          <Grid columns="equal">
+            <Grid.Column width={8}>
+              <Header as="h1">Sales Tracking</Header>
+            </Grid.Column>
+            <Grid.Column textAlign="right">
+              <Button.Group>
+                <Button
+                  icon
+                  labelPosition="left"
+                  negative
+                  size="small"
+                  onClick={this.removeItem}
+                >
+                  <Icon name="close"></Icon>
+                  Remove
+                </Button>
+              </Button.Group>
+            </Grid.Column>
+          </Grid>
+        </div>
+
+        <Divider />
+
+        <div>
+          <Table sortable celled definition structured>
+            <Table.Header>
+              <Table.Row textAlign="center">
+                <Table.HeaderCell collapsing />
+                <Table.HeaderCell width={2}>Date</Table.HeaderCell>
+                <Table.HeaderCell width={3}>Customer</Table.HeaderCell>
+                <Table.HeaderCell width={3}>Comments</Table.HeaderCell>
+                <Table.HeaderCell width={6}>Items</Table.HeaderCell>
+                <Table.HeaderCell width={2}>Labor Rate</Table.HeaderCell>
+                <Table.HeaderCell width={2}>Total Labor</Table.HeaderCell>
+                <Table.HeaderCell width={2}>Mark Up</Table.HeaderCell>
+                <Table.HeaderCell width={2}>Total Cost</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            {data.map(items => {
+              return (
+                <Table.Body>
+                  <Table.Row key={items.SalesId}>
+                    <Table.Cell collapsing>
+                      <Checkbox id={items.SalesId} onClick={this.toggleCheck} />
+                    </Table.Cell>
+                    <Table.Cell textAlign="center">{items.soldDate}</Table.Cell>
+                    <Table.Cell textAlign="center">{items.name}</Table.Cell>
+                    <Table.Cell textAlign="center">{items.comment}</Table.Cell>
+                    <Table.Cell>
+                      {items.items
+                        .map(element => element.quantity + " " + element.name)
+                        .join(", ")}
+                    </Table.Cell>
+                    <Table.Cell textAlign="center">
+                      ${items.laborRate}/hr
+                    </Table.Cell>
+                    <Table.Cell textAlign="center">
+                      {items.totalLabor}
+                    </Table.Cell>
+                    <Table.Cell textAlign="center">{items.markUp}%</Table.Cell>
+                    <Table.Cell textAlign="center">
+                      ${items.orderCost}
+                    </Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              );
+            })}
+          </Table>
+        </div>
+      </Segment>
+    );
   }
 }
-//   render() {
-//     return <div></div>;
-//   }
-// }
 
-// export default Sales;
+export default Sales;
