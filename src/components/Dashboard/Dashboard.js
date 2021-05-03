d2387c4299d9a037968ac530cc3301962b5ed2eb
Denil <denilpoudel@gmail.com>
2021-05-03 00:04:06 -0700

    signup creates userprofile now

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,14 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 import { AuthContext } from "../App/Auth";
 import { Tab, Segment } from "semantic-ui-react";
-import Sales from "../Budget/Sales";
+import { db } from "../Firebase";
 
 const Dashboard = () => {
   const { currentUser } = useContext(AuthContext);
+
+  const [data, setData] = useState(null);
+
   if (!currentUser) {
     return <Redirect to="/" />;
   }
+
+  useEffect(() => {
+    let docs = [];
+    db.collection("users")
+      .doc("aRHNBBjHoC5WlghIbwYN")
+      .collection("items")
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          docs.push({ id: doc.id, ...doc.data() });
+        });
+      })
+      .then(() => {
+        setData(docs);
+      });
+  }, []);
+
+  console.log(data);
 
   return (
     <div>
